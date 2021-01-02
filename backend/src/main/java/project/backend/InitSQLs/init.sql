7c24bf6136093bee56b73c90b50ee081caa1fb20
zhshy <18302010080@fudan.edu.cn>
2021-01-02 15:39:09 +0800

    [Add] add life_status to patient, and add 4 users to db

--- backend/src/main/java/project/backend/InitSQLs/init.sql
+++ backend/src/main/java/project/backend/InitSQLs/init.sql
@@ -23,7 +23,6 @@ insert into doctor(id, name, password, age, gender) VALUES ('D002', 'doctor2', '
 insert into doctor(id, name, password, age, gender) VALUES ('D003', 'doctor3', '123456', 28, 'female');
 
 
-drop table if exists bed;
 drop table if exists chief_nurse;
 create table if not exists chief_nurse
 (
@@ -65,9 +64,9 @@ create table if not exists hospital_nurse
     current_resp_num int not null default 0 check ( current_resp_num >= 0 and current_resp_num <= 3 ),
     primary key (id)
 )charset = utf8;
-insert into hospital_nurse(id, name, password, age, gender) VALUES ('E001', 'emergency_nurse1', '123456', 26, 'female');
-insert into hospital_nurse(id, name, password, age, gender) VALUES ('E002', 'emergency_nurse2', '123456', 36, 'male');
-insert into hospital_nurse(id, name, password, age, gender) VALUES ('E003', 'emergency_nurse3', '123456', 28, 'female');
+insert into hospital_nurse(id, name, password, age, gender) VALUES ('H001', 'hospital_nurse1', '123456', 26, 'female');
+insert into hospital_nurse(id, name, password, age, gender) VALUES ('H002', 'hospital_nurse2', '123456', 36, 'male');
+insert into hospital_nurse(id, name, password, age, gender) VALUES ('H003', 'hospital_nurse3', '123456', 28, 'female');
 
 
 create table if not exists treatment_region
@@ -96,15 +95,16 @@ create table if not exists patient
     gender varchar(10) default 'male' check ( gender in ('male', 'female') ),
     age int not null check ( age > 0 ),
     disease_level int not null check ( disease_level >= 1 and disease_level <= 3 ),
+    life_status varchar(10) not null check ( life_status in ('healthy', 'treating', 'dead') ),
     nurse_id varchar(20),
     treatment_region_level int not null,
     primary key (patient_id),
     foreign key (nurse_id) references chief_nurse(id),
     foreign key (treatment_region_level) references treatment_region(level)
 )charset = utf8;
-insert into patient(name, age, disease_level, nurse_id, treatment_region_level) VALUES ('p1', 20, 1, 'C001', 1);
-insert into patient(name, age, disease_level, nurse_id, treatment_region_level) VALUES ('p2', 20, 2, 'C002', 2);
-insert into patient(name, age, disease_level, nurse_id, treatment_region_level) VALUES ('p3', 20, 3, 'C003', 3);
+insert into patient(name, age, disease_level, life_status, nurse_id, treatment_region_level) VALUES ('p1', 20, 1, 'treating', 'C001', 1);
+insert into patient(name, age, disease_level, life_status, nurse_id, treatment_region_level) VALUES ('p2', 20, 2, 'treating', 'C002', 2);
+insert into patient(name, age, disease_level, life_status, nurse_id, treatment_region_level) VALUES ('p3', 20, 3, 'treating', 'C003', 3);
 
 
 create table if not exists bed
@@ -125,6 +125,7 @@ create table if not exists checklist
     id int auto_increment not null,
     test_result varchar(10) not null check ( test_result in ('positive', 'negative') ),
     date datetime not null,
+    # ToDo: 这个病情评级和上边病人的病情评级严格来说没有 reference 的必要，到时候如果这边有新的记录记得同步病人的病情评级属性
     disease_level int not null check ( disease_level >= 1 and disease_level <= 3 ),
     doctor_id varchar(20) not null,
     patient_id int not null,
@@ -155,4 +156,29 @@ insert into patient_status(temperature, symptom, life_status, date, patient_id, 
             VALUES (37.6, 'fever', 'treating', '2020-12-20 13:30:20', 3, 'C001', 1);
 insert into patient_status(temperature, symptom, life_status, date, patient_id, nurse_id, checklist_id)
             VALUES (37.1, 'healthy', 'treating', '2020-12-25 13:30:20', 1, 'C001', 2);
+
+drop user 'doctor'@'localhost';
+create user 'doctor'@'localhost' identified by '123456';
+grant select, update on patient to 'doctor'@'localhost';
+grant select on chief_nurse to 'doctor'@'localhost';
+grant select on hospital_nurse to 'doctor'@'localhost';
+grant select, insert on checklist to 'doctor'@'localhost';
+
+drop user 'chief_nurse'@'localhost';
+create user 'chief_nurse'@'localhost' identified by '123456';
+grant select on patient to 'chief_nurse'@'localhost';
+grant select, insert, delete on hospital_nurse to 'chief_nurse'@'localhost';
+grant select on bed to 'chief_nurse'@'localhost';
+
+drop user 'emergency_nurse'@'localhost';
+create user 'emergency_nurse'@'localhost' identified by '123456';
+grant select, insert on patient to 'emergency_nurse'@'localhost';
+grant select, insert on patient_status to 'emergency_nurse'@'localhost';
+grant select on treatment_region to 'emergency_nurse'@'localhost';
+
+drop user 'hospital_nurse'@'localhost';
+create user 'hospital_nurse'@'localhost' identified by '123456';
+grant select on patient to 'hospital_nurse'@'localhost';
+grant select, update on checklist to 'hospital_nurse'@'localhost';
+grant select, update, insert on patient_status to 'hospital_nurse'@'localhost';
 
--- backend/src/main/java/project/backend/Utils/Config.java
+++ backend/src/main/java/project/backend/Utils/Config.java
@@ -10,4 +10,17 @@ public class Config {
     public static String ROOT = "root";
     public static String PASSWORD = "zsy666";
     public static String DB_URL = "jdbc:mysql://localhost/database_project";
+
+    // 4 种身份
+    public static String DOCTOR = "doctor";
+    public static String PASSWORD_D = "123456";
+
+    public static String CHIEF_NURSE = "chief_nurse";
+    public static String PASSWORD_C = "123456";
+
+    public static String EMERGENCY_NURSE = "emergency_nurse";
+    public static String PASSWORD_E = "123456";
+
+    public static String HOSPITAL_NURSE = "hospital_nurse";
+    public static String PASSWORD_H = "123456";
 }
